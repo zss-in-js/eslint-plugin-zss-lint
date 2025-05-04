61dfd0f97af5b18bf54be7bae3e72a365af201dc
refirst11 <ocelot2828@gmail.com>
2025-05-04 20:57:51 +0900

    test(sort-properties.test.js): Corrected according to warning message

--- __tests__/sort-properties.test.js
+++ __tests__/sort-properties.test.js
@@ -17,22 +17,22 @@ ruleTester.run('sort-properties', rule, {
       code: 'const styles = { display: "block", position: "absolute", top: "0", right: "0", bottom: "0", left: "0" };',
       errors: [
         {
-          message: 'Property "display" is out of order. Expected position: 6.',
+          message: 'Property "display" should be at position 6',
         },
         {
-          message: 'Property "position" is out of order. Expected position: 1.',
+          message: 'Property "position" should be at position 1',
         },
         {
-          message: 'Property "top" is out of order. Expected position: 2.',
+          message: 'Property "top" should be at position 2',
         },
         {
-          message: 'Property "right" is out of order. Expected position: 3.',
+          message: 'Property "right" should be at position 3',
         },
         {
-          message: 'Property "bottom" is out of order. Expected position: 4.',
+          message: 'Property "bottom" should be at position 4',
         },
         {
-          message: 'Property "left" is out of order. Expected position: 5.',
+          message: 'Property "left" should be at position 5',
         },
       ],
       output:
